e5ef63abc29b4299c1922a802a3b27ead1579ecc
deeksh20 <144101595+deeksh20@users.noreply.github.com>
2024-02-01 19:13:44 +0530

    test

--- set01/problem07.c
+++ set01/problem07.c
@@ -6,7 +6,7 @@ int sum_n_nos(int n);
 void output(int n, int sum);
 int input_n() {
     int n;
-    printf("enter the number");
+    printf("enter the value of n");
     scanf("%d",&n);
     return n;
 }
@@ -14,14 +14,15 @@ int sum_n_nos(int n) {
     int sum = 0;
     for(int i=1;i<=n;i++)
     {
-        sum=sum+i;
+        sum+=i;
     }
+    return sum;
 }
 void output(int n, int sum) {
-    printf("the sum of natural numbers is %d",n,sum);
+    printf("the sum of all  natural numbers until %d is %d/n",n,sum);
 }
 int main(){
-    int n;
+    int n = input_n();
     int sum =sum_n_nos(n);
     output(n,sum);
     return 0;
