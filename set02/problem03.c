439497601bdf593b2de746532d29f02ca0ba40bc
deeksh20 <144101595+deeksh20@users.noreply.github.com>
2024-02-15 22:49:16 +0530

    test

---
+++ set02/problem03.c
@@ -0,0 +1,35 @@
+//03.  Write a program find whether a number is a composite number
+
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+int main(){
+    int number,result;
+    number=input_number();
+    result=is_composite(number);
+    output(number,result);
+    return 0;
+}
+int input_number(){
+    int num;
+    printf("enter the composite number");
+    scanf("%f",&num);
+    return num;
+}
+int is_composite(int n){
+    int i ,factors=0;
+    for(i=1;i<=n;i++){
+        if(n%i == 0){
+            factors++;
+        }
+    }
+    return (factors > 2);
+}
+void output(int n, int result){
+    if(result){
+        printf("%d is a composite numbers\n",n);  
+    }
+    else{
+        printf("%d is not a composite number\n",n);
+    }
+}
