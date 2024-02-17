9981307283c361505ee133b6e31d6fd45247b9ac
deeksh20 <144101595+deeksh20@users.noreply.github.com>
2024-02-17 20:05:34 +0530

    test

--- set02/problem03.c
+++ set02/problem03.c
@@ -36,3 +36,4 @@ void output(int n, int result){
         printf("%d is not a composite number\n",n);
     }
 }
+        
---
+++ set02/problem04.c
@@ -0,0 +1,12 @@
+
+//04.  Write a program to find Sum of composite number in an array of different numbers entered by the user.
+
+#include<stdio.h>
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+int main(){
+    int n,
+
+}
