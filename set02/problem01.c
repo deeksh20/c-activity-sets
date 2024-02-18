ebe5ef8a525cb98dfafe119c393a748ceb5e29cd
deeksh20 <144101595+deeksh20@users.noreply.github.com>
2024-02-18 21:05:05 +0530

    test

--- set02/problem01.c
+++ set02/problem01.c
@@ -19,7 +19,7 @@ void input(float *base, float *height){
     scanf("%f",height);
 
 }
-void find_area(float base , float height, float *area) {
+void find_area(float base , float height, float *area)  {
    *area = 0.5 * base * height;
 }
 
