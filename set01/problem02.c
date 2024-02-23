a358bfd8ed358946a362cbd69804da96d73e44dd
deeksh20 <144101595+deeksh20@users.noreply.github.com>
2024-02-23 14:02:17 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,5 +1,6 @@
 //2. Write a C program to add two numbers.
 #include<stdio.h>
+
 int main()
 {
     int a,b,sum;
--- set01/problem03.c
+++ set01/problem03.c
@@ -24,3 +24,4 @@ int add(int a, int b){
 void output(int a, int b, int sum){
     printf("the sum of %d and %d is %d\n",a,b,sum);
 }
+
